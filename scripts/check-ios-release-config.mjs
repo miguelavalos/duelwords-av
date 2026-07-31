53e01dd79934c44f223c31b2d6bb14e72cf6f4b7
Miguel Angel Avalos Fernandez <miguelavalos@gmail.com>
2026-07-31 10:12:58 +0200

    chore: reserve ios build 12

--- scripts/check-ios-release-config.mjs
+++ scripts/check-ios-release-config.mjs
@@ -56,7 +56,7 @@ expectEqual('expo.slug', expoConfig.slug, 'duelwords-av');
 expectEqual('expo.version', expoConfig.version, '0.1.0');
 expectEqual('expo.orientation', expoConfig.orientation, 'portrait');
 expectEqual('expo.ios.bundleIdentifier', expoConfig.ios?.bundleIdentifier, expectedBundleIdentifier);
-expectEqual('expo.ios.buildNumber', expoConfig.ios?.buildNumber, '11');
+expectEqual('expo.ios.buildNumber', expoConfig.ios?.buildNumber, '12');
 expectEqual('expo.ios.supportsTablet', expoConfig.ios?.supportsTablet, true);
 expectEqual('expo.ios.requireFullScreen', expoConfig.ios?.requireFullScreen, true);
 expectEqual(
